906590e52f561b1e36abea25092d716cccd31dd4
Mark Sakauye <msakauye@uthsc.edu>
2018-02-17 17:52:55 -0500

    empty pages and routes

---
+++ src/components/Resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h3>protected shiz</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
